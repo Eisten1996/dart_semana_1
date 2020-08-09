0835fd09711bdd7a86111430e289d07e93849ef1
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-08-09 01:28:41 -0500

    agregacion de los tipos de datos

---
+++ 02_tipo_de_datos.dart
@@ -0,0 +1,49 @@
+void main() {
+
+  /**Numeros**/
+  int numero1 = 35;
+  print(numero1);
+  double numero2 = 35.61;
+  print(numero2);
+
+  /**Strings**/
+  String texto1 = "Con comilla doble";
+  print(texto1);
+  String texto2 = 'Con comilla simple';
+  String textoParrafo = '''
+  Esto es un parrafo
+  casjhdkjsa
+  ''';
+
+  /**Boolean**/
+  bool flag = true;
+  print(flag);
+  bool noflag = false;
+  print(noflag);
+
+  /**List**/
+  List<String> listaNombres = ["Eisten", "Alisson", "Luis"];
+  print(listaNombres);
+  listaNombres.add("kjshda");
+  print(listaNombres);
+
+  List<String> listaApellidos = new List(4);
+  print(listaApellidos);
+  listaApellidos[0] = "Eisten";
+  print(listaApellidos);
+
+  /**Set**/
+  Set<String> listaNombres2 = {"Eisten", "Alisson", "Luis"};
+  print(listaNombres2);
+  listaNombres2.add("kjshda");
+  print(listaNombres2);
+  
+  /**Map**/
+  Map persona = {
+    'nombre':'Eisten',
+    'edad': 24,
+    'apellido':'Flores',
+  };
+  print(persona);
+  print(persona[edad]);
+}
